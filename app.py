c1e0b515d5c325b04587ccf3f41b1d04f211f485
desurkumaran777 <desurkumaran777@gmail.com>
2024-07-13 04:48:06 +0530

    Crated New Model CartModel and it's required views and routes.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
-from models import ProductModel, db
+from models import ProductModel, db, CartModel
 
 app = Flask(__name__)
 
@@ -57,8 +57,52 @@ class ProductView(Resource):
             return product.json(), 201
 
 
+class CartsView(Resource):
+    def get(self):
+        products = CartModel.query.all()
+        return {'Carts': list(prod.json() for prod in products)}, 200
+
+    def post(self):
+        data = request.get_json()
+        new_prod = CartModel(
+            data.get('prodId'), data.get('prodName'), data.get('prodDesc'), data.get('prodBrand'), data.get('prodPrice'))
+        db.session.add(new_prod)
+        db.session.commit()
+        return new_prod.json(), 201
+
+
+class CartView(Resource):
+    def get(self, prod_id):
+        product = CartModel.query.get_or_404(prod_id)
+        return product.json(), 200
+
+    def delete(self, prod_id):
+        product = CartModel.query.get_or_404(prod_id)
+        db.session.delete(product)
+        db.session.commit()
+        return {'message': 'deleted successfully'}
+
+    def put(self, prod_id):
+        data = request.get_json()
+        product = CartModel.query.filter_by(prod_id=prod_id).first()
+        if product:
+            product.prod_name = data.get('prodName')
+            product.prod_desc = data.get('prodDesc')
+            product.prod_brand = data.get('prodBrand')
+            product.prod_price = data.get('prodPrice')
+            product.order_qty = data.get('orderQty')
+            product.is_added_to_cart = data.get('isAddedToCart')
+            db.session.commit()
+            return product.json(), 200
+
+        else:
+            return {'message': 'cart is not found'}, 404
+
+
 api.add_resource(ProductsView, '/products')
 api.add_resource(ProductView, '/product/<int:prod_id>')
+api.add_resource(CartsView, '/carts')
+api.add_resource(CartView, '/cart/<int:prod_id>')
 
 app.debug = True
 
--- models.py
+++ models.py
@@ -4,7 +4,6 @@ db = SQLAlchemy()
 
 
 class ProductModel(db.Model):
-    __tablename__ = 'products'
 
     prod_id = db.Column(db.Integer, primary_key=True)
     prod_name = db.Column(db.String(100), nullable=False)
@@ -15,6 +14,31 @@ class ProductModel(db.Model):
     is_added_to_cart = db.Column(db.Boolean, nullable=False)
 
     def __init__(self, name, desc, brand, price, order_qty=1, is_added_to_cart=False):
+        self.prod_name = name
+        self.prod_desc = desc
+        self.prod_brand = brand
+        self.prod_price = price
+        self.order_qty = order_qty
+        self.is_added_to_cart = is_added_to_cart
+
+    def json(self):
+        return (
+            {'prodId': self.prod_id, 'prodName': self.prod_name, 'prodDesc': self.prod_desc, 'prodBrand': self.prod_brand,
+             'prodPrice': self.prod_price, 'orderQty': self.order_qty, 'isAddedToCart': self.is_added_to_cart})
+
+
+class CartModel(db.Model):
+
+    prod_id = db.Column(db.Integer, primary_key=True)
+    prod_name = db.Column(db.String(100), nullable=False)
+    prod_desc = db.Column(db.String(255), nullable=False)
+    prod_brand = db.Column(db.String(50), nullable=False)
+    prod_price = db.Column(db.Integer, nullable=False)
+    order_qty = db.Column(db.Integer, nullable=False)
+    is_added_to_cart = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, prod_id, name, desc, brand, price, order_qty=1, is_added_to_cart=True):
+        self.prod_id = prod_id
         self.prod_name = name
         self.prod_desc = desc
         self.prod_brand = brand
